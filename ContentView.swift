ae9e86bd3c07a33bcb2563ca5bcd914d24c3baed
Subham <spathak5540@stu.d214.org>
2023-11-30 10:49:59 -0600

    febwkhjfgmbwliv

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack{
+                Color.indigo
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Text("Player Comparisons (Young Talent) ")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    Image(systemName: "soccerball")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    
+                        .padding()
+                        .padding()
+                    
+                    NavigationLink("Page 1") {
+                        Page1()
+                    }
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))
+                    
+                    .padding()
+                    
+                    
+                    
+                }
+            }
         }
     }
 }
--- Page1.swift
+++ Page1.swift
@@ -4,5 +4,13 @@
 //
 //  Created by Rahul A. Basu on 11/30/23.
 //
+import SwiftUI
 
 import Foundation
+struct Page1:  View {
+    var body: some View {
+        VStack{
+            Text("Subham")
+        }
+    }
+}
