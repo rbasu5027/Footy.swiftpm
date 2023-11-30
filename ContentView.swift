c2dd26a21b5f41b80eaffcb5c8eb28212528cb8d
Subham <spathak5540@stu.d214.org>
2023-11-30 10:57:46 -0600

    kuhjki

--- ContentView.swift
+++ ContentView.swift
@@ -2,34 +2,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView{
-            ZStack{
-                Color.indigo
-                    .ignoresSafeArea()
+        ZStack{
+            Color.indigo
+                .ignoresSafeArea()
+            
+            VStack {
                 
-                VStack {
-                    Text("Player Comparisons (Young Talent) ")
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
-                    Image(systemName: "soccerball")
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
-                    
-                        .padding()
-                        .padding()
-                    
-                    NavigationLink("Page 1") {
-                        Page1()
-                    }
+                Text("Player Comparisons (Young Talent) ")
+                    .font(.largeTitle)
                     .foregroundColor(.white)
-                    .font(.system(size: 24))
-                    
+                Image(systemName: "soccerball")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
                     .padding()
-                    
-                    
-                    
+                    .padding()
+                
+                Button(action: {
+                    Page1.toggle()
+                }) {
+                    Text("Go to Another View")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .sheet(isPresented: Page1) {
+                    Page1()
                 }
             }
         }
+        
     }
 }
